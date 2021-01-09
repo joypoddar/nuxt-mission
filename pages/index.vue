be8156f87ff052d3776a659f3a35d3a1dec905ee
Joy Poddar <joypoddar.sd@gmail.com>
2021-01-09 22:17:13 +0530

    feat: nuxt Generate and Deploy

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">nuxt-mission</h1>
-      <button @click="showPlanets">Show Planets</button>
-      <div v-if="show">
-        <LazyPlanetsList />
-      </div>
+      <PlanetsList />
     </div>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      show: false,
-    }
-  },
-  methods: {
-    showPlanets() {
-      this.show = true
-    },
-  },
-}
-</script>
